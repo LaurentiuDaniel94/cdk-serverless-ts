89dbeb35f0a72ccf7d946c45095f8d8c553bb063
Laurentiu Daniel Ginghina <laurentiuginghina@outlook.com>
2023-12-10 12:41:39 +0200

    esbuild

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,7 +1,7 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
-import {Construct} from 'constructs'
-import {Code, Runtime} from 'aws-cdk-lib/aws-lambda'
-import {join} from 'path'
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { join } from 'path'
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
@@ -19,7 +19,7 @@ export class LambdaStack extends Stack {
 
       const helloLambda = new NodejsFunction(this, 'HelloLambda', {
         runtime: Runtime.NODEJS_18_X,
-        handler: 'hello.main',
+        handler: 'handler',
         entry: (join(__dirname, '..', '..', 'services', 'hello.ts')),
         environment: {
           TABLE_NAME: props.dynamoTable.tableName
